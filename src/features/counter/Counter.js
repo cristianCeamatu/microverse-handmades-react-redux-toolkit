f43db5b4133b43afa54b7738255ccb9412fdc432
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-11-16 10:46:48 +0200

    Add linters

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -42,9 +42,7 @@ export function Counter() {
         />
         <button
           className={styles.button}
-          onClick={() =>
-            dispatch(incrementByAmount(Number(incrementAmount) || 0))
-          }
+          onClick={() => dispatch(incrementByAmount(Number(incrementAmount) || 0))}
         >
           Add Amount
         </button>
